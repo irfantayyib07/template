015be691e6bfaa80aaf3f2242fa1ac67b92f70e6
farhankhanphp <farhankhan.php8@gmail.com>
2024-06-24 12:31:43 +0500

    Modify MultiSelect and add Search bar in MultiSelectCheckmakrs

--- src/components/multiple-select-checkmarks/index.tsx
+++ src/components/multiple-select-checkmarks/index.tsx
@@ -1,12 +1,10 @@
 import * as React from "react";
-import OutlinedInput from "@mui/material/OutlinedInput";
-import InputLabel from "@mui/material/InputLabel";
-import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
-import ListItemText from "@mui/material/ListItemText";
-import Select, { SelectChangeEvent } from "@mui/material/Select";
+import Autocomplete from "@mui/material/Autocomplete";
+import TextField from "@mui/material/TextField";
 import Checkbox from "@mui/material/Checkbox";
 import { useFormikContext } from "formik";
+import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
+import CheckBoxIcon from "@mui/icons-material/CheckBox";
 
 interface Record {
   name: string;
@@ -19,55 +17,33 @@ interface Props {
   records: Record[];
 }
 
-const ITEM_HEIGHT = 48;
-const ITEM_PADDING_TOP = 8;
-const MenuProps = {
-  PaperProps: {
-    style: {
-      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-      width: 250,
-    },
-  },
-};
-
 const MultipleSelectCheckmarks: React.FC<Props> = ({ name, label, records }) => {
   const { values, setFieldValue } = useFormikContext<{ [key: string]: Record[] }>();
   const selectedRecords = values[name] || [];
 
-  if (values[name] === undefined) return;
-
-  const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
-    const { value } = event.target;
-    setFieldValue(name, value as Record[]);
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: Record[]) => {
+    setFieldValue(name, newValue);
   };
 
   return (
-    <div>
-      <FormControl fullWidth>
-        <InputLabel id={`${name}-label`}>{label}</InputLabel>
-        <Select
-          labelId={`${name}-label`}
-          id={name}
-          multiple
-          value={selectedRecords}
-          //@ts-ignore
-          onChange={handleChange}
-          input={<OutlinedInput label={label} />}
-          renderValue={selected =>
-            (selected as Record[]).map(record => `${record.name} (${record.price})`).join(", ")
-          }
-          MenuProps={MenuProps}
-        >
-          {records.map(record => (
-            //@ts-ignore
-            <MenuItem key={record.name} value={record}>
-              <Checkbox checked={selectedRecords.some(item => item.name === record.name)} />
-              <ListItemText primary={`${record.name} (${record.price})`} />
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-    </div>
+    <Autocomplete
+      multiple
+      id={name}
+      options={records}
+      disableCloseOnSelect
+      getOptionLabel={option => `${option.name} (${option.price})`}
+      value={selectedRecords}
+      onChange={handleChange}
+      renderOption={(props, option, { selected }) => (
+        <li {...props}>
+          <Checkbox style={{ marginRight: 8 }} checked={selected} />
+          <span>{`${option.name} (${option.price})`}</span>
+        </li>
+      )}
+      renderInput={params => (
+        <TextField {...params} variant="outlined" label={label} placeholder={`Select ${label}`} />
+      )}
+    />
   );
 };
 
